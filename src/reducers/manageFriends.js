6d220108ba649825555a3cbdb5560299695d24f4
Milan Dhar <milan.dhar@gmail.com>
2020-09-23 20:59:59 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case('ADD_FRIEND'):
+    let newFriends = state.friends.slice()
+    newFriends.push(action.friend)
+    return {...state, friends: newFriends};
+
+    case('REMOVE_FRIEND'):
+    let foo = state.friends.slice()
+    foo = foo.filter((friend) => friend.id !== action.id)
+    
+    return {...state, friends: foo};
+  default:
+    return state
+  }
 }
