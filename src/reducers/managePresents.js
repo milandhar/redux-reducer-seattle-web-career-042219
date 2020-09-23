642b87fbc2502fd01996497698902d00dcd6aced
Milan Dhar <milan.dhar@gmail.com>
2020-09-23 20:20:22 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -2,6 +2,7 @@ export function managePresents(state = {numberOfPresents: 0}, action){
   let currNum = state.numberOfPresents;
   switch(action.type){
     case ("Increase"):
+    console.log(currNum)
       return {numberOfPresents: currNum + 1};
 
     default:
