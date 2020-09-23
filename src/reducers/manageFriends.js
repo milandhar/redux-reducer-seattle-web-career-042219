5fb7c4020a3da5e7c5156c11bd80b01f67d6c8ea
Milan Dhar <milan.dhar@gmail.com>
2020-09-23 20:50:19 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,8 +1,6 @@
 export function manageFriends(state, action){
   switch(action.type){
     case('ADD_FRIEND'):
-    console.log(action)
-    console.log(state)
     let newFriends = state.friends.slice()
     newFriends.push(action.friend)
     return {...state, friends: newFriends};
